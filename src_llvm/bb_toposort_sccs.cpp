3b6109d176ffa7808970e1b958bb21296761fb29
Sander Kersten <spkersten@gmail.com>
2014-08-30 21:08:06 +0200

    Changed code because parseIRFile now returns a unique_ptr

--- src_llvm/bb_toposort_sccs.cpp
+++ src_llvm/bb_toposort_sccs.cpp
@@ -160,7 +160,7 @@ int main(int argc, char **argv) {
 
   // Parse the input LLVM IR file into a module.
   SMDiagnostic Err;
-  Module *Mod = ParseIRFile(argv[2], Err, getGlobalContext());
+  std::unique_ptr<Module> Mod = parseIRFile(argv[2], Err, getGlobalContext());
   if (!Mod) {
     Err.print(argv[0], errs());
     return 1;
